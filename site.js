f98b1c49074f08a01deb359bfe224615393b2921
wangqinglu-on-macos <18353184289@163.com>
2018-02-08 18:19:24 +0800

    Add animation effects

--- site.js
+++ site.js
@@ -6,3 +6,5 @@
 {% include_relative _assets/js/security.deobfuscate_email.js %}
 {% include_relative _assets/js/ui.sliding_nav.js %}
 {% include_relative _assets/js/ui.hiding_header.js %}
+{% include_relative _assets/js/particles.js %}
+{% include_relative _assets/js/app.js %}
